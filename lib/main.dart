d1cd8fabb9018d9dd1fe25c6e65d6b6182923f9a
bhogalprabjot <bhogalprabjot1@gmail.com>
2019-12-28 00:19:55 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -40,8 +40,35 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
             children: transactions.map(
-              () {}.toList(),
-            ),
+              (tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(tx.amount.toString()),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(tx.title),
+                          Text(tx.date.toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ).toList(),
           ),
         ],
       ),
