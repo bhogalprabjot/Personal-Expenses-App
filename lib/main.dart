a8df38e972d2c188d2492256b6efb67e0162403c
bhogalprabjot <bhogalprabjot1@gmail.com>
2019-12-28 12:34:16 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -51,17 +51,34 @@ class MyHomePage extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                           border: Border.all(
-                            color: Colors.black,
+                            color: Colors.purple,
                             width: 2,
                           ),
                         ),
                         padding: EdgeInsets.all(10),
-                        child: Text(tx.amount.toString()),
+                        child: Text(
+                          '₹ ${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(tx.title),
-                          Text(tx.date.toString()),
+                          Text(
+                            '{tx.title}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ],
                       ),
                     ],
